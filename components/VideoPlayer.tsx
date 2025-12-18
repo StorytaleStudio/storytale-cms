88f7a57367e10ee62e8e1e92feb63571b952b1ac
Josh Tregenza <josh@joshtregenza.com>
2025-12-18 10:08:17 +1000

    updates to the about video

--- components/VideoPlayer.tsx
+++ components/VideoPlayer.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import styles from './style/videoplayer.module.css'
+import TVOutArea from './svgs/tv-outer'
+import TVInArea from './svgs/tv-inner'
 
 interface VideoPlayerProps {
   videoUrl: string
@@ -8,9 +10,11 @@ interface VideoPlayerProps {
 export const VideoPlayer: React.FC<VideoPlayerProps> = ({ videoUrl }) => {
   return (
     <div className={styles.outerArea}>
+      <TVOutArea />
       <div className={styles.innerArea}>
+        <TVInArea />
         <div className={styles.videoPlayerWrapper}>
-          <video key={videoUrl} className={styles.video} controls autoPlay>
+          <video key={videoUrl} className={styles.video} controls muted autoPlay loop>
             <source src={videoUrl} type="video/mp4" />
             Your browser does not support the video tag.
           </video>
