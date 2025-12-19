8a15e5c2ca3b0b897f3cb02b98522529394e99f6
Josh Tregenza <josh@joshtregenza.com>
2025-12-19 12:46:49 +1000

    Update FantasyBackground.tsx

--- components/FantasyBackground.tsx
+++ components/FantasyBackground.tsx
@@ -630,7 +630,7 @@ export default function FantasyBackground({
           inset: 0,
           width: '100%',
           height: '100%',
-          mixBlendMode: 'color-dodge',
+          mixBlendMode: 'color-burn',
           pointerEvents: 'none',
           display: 'block',
           zIndex: -1,
